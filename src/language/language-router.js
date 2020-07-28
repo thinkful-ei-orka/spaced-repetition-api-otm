5b28ec01f664bc020cf02f021492b91b18ebf233
Taeil Kwak <taeil2@gmail.com>
2020-07-28 12:46:12 -0500

    finished typos in guess

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -53,17 +53,21 @@ languageRouter
 languageRouter
   .post('/guess', async (req, res, next) => {
     try {
-      for (const [key, value] of Object.entries(req.body))
-      if (value == null || key !== 'guess') {
-        return res.status(400).json({error: `Missing 'guess' in request body`});
-    }
-      const words = await LanguageService.getLanguageWords(
-        req.app.get('db'),
-        req.language.id,
-      )
-    
-    res.send('implement me!')
+      for (const [key, value] of Object.entries(req.body)) {
+        if (value == null || key !== 'guess') {
+          return res.status(400).json({error: `Missing 'guess' in request body`});
+        }
       }
+    } catch (e) {
+      console.log(e);
+    }
+
+    const words = await LanguageService.getLanguageWords(
+      req.app.get('db'),
+      req.language.id,
+    )
+
+    res.send('implement me!')
   })
 
 module.exports = languageRouter
